a2b2e0435f6f598dbfa040803d41508567b0294a
Chirag Gour <chirag.gour@skillizee.com>
2026-04-16 16:49:16 +0530

    Initial commit: Clean and Interactive Galaxy background simulation

---
+++ rollup.config.js
@@ -0,0 +1,68 @@
+import { babel } from '@rollup/plugin-babel';
+import glsl from 'rollup-plugin-glsl';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import livereload from 'rollup-plugin-livereload';
+import ts from '@rollup/plugin-typescript';
+import serve from 'rollup-plugin-serve';
+import image from '@rollup/plugin-image';
+import typescript from 'typescript';
+
+const env = process.env.__ENV;
+const production = env === 'production';
+const devOutput = {
+  sourcemap: true,
+  file: 'lib/index.js',
+  format: 'iife'
+};
+const productionOutput = [
+  {
+    file: 'lib/bundle-esm.js',
+    format: 'esm'
+  }
+];
+
+const config = {
+  input: production ? 'src/index.ts' : 'src/app.ts',
+  output: production ? productionOutput : devOutput,
+  plugins: [
+    image(),
+    glsl({
+      // By default, everything gets included
+      include: 'src/**/*.glsl',
+
+      // Undefined by default
+      exclude: ['**/index.html'],
+
+      // Source maps are on by default
+      sourceMap: true
+    }),
+    ts({
+      typescript,
+      tsconfig: production ? 'ts/es.tsconfig.json' : 'tsconfig.json'
+    }),
+    !production && sourcemaps(),
+    nodeResolve(
+      production
+        ? {
+            jail: 'src'
+          }
+        : undefined
+    ),
+    !production && babel({ babelHelpers: 'bundled' }),
+    !production &&
+      serve({
+        port: 3000
+      }),
+    !production &&
+      livereload({
+        watch: 'lib',
+        verbose: true, // Disable console output
+
+        // other livereload options
+        port: 35729
+      })
+  ]
+};
+
+export default config;
---
+++ src/examples/basic.ts
@@ -0,0 +1,29 @@
+import Galaxy from 'Galaxy';
+// @ts-expect-error
+import { OrbitControls } from '../helpers/OrbitControls';
+
+// @ts-expect-error
+import particleTexture from '../assets/particle-example.png';
+
+const canvas = document.createElement('canvas');
+
+function basicExample(): void {
+  const galaxy = new Galaxy({
+    canvas: document.querySelector('#c') ?? canvas,
+    window,
+    layers: [
+      {
+        color: '#FFFFFF',
+        texture: particleTexture,
+        count: 100000
+      }
+    ]
+  });
+
+  // eslint-disable-next-line no-new
+  new OrbitControls(galaxy.camera, document.querySelector('#c'));
+  galaxy.render();
+  galaxy.play();
+};
+
+export default basicExample;
